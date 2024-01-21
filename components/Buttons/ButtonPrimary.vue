00dee93d868bf84036fcfd681936c5c61196da7d
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-21 19:35:26 -0300

    ADD: Button pattern

---
+++ components/Buttons/ButtonPrimary.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="flex items-center bg-[#1b96f3] text-white px-5 py-3 rounded-full gap-2 mt-8 w-full justify-center"
+  >
+    <PlusIcon class="h-6 w-6 text-white" />
+    <p>{{ title }}</p>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import { PlusIcon } from "@heroicons/vue/24/solid";
+const { to, title } = defineProps({
+  to: { type: String, default: "/" },
+  title: { type: String, default: "Criar anotação" },
+});
+</script>
---
+++ pages/addnote/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <form class="flex flex-col justify-between gap-2 w-full h-full slide-top">
+    <div class="flex flex-col gap-1">
+      <Input type="text" />
+      <Input type="text" title="Potencial de negocio" placeholder="0" />
+
+      <Input
+        type="text"
+        title="Categorização"
+        placeholder="importante, não importante, bom, ruim ..."
+      >
+        <CalendarDaysIcon class="h-6 w-6 text-gray-500" />
+      </Input>
+
+      <Input type="date" title="Lembrete" placeholder="20 de agosto 2023">
+        <CalendarDaysIcon  class="h-6 w-6 text-gray-500" />
+      </Input>
+    </div>
+    <ButtonPrimary title="Salvar" />
+  </form>
+</template>
+
+<script setup lang="ts">
+import Input from "@@/components/inputs/Input.vue";
+import ButtonPrimary from "@@/components/Buttons/ButtonPrimary.vue";
+import { CalendarDaysIcon } from "@heroicons/vue/24/outline";
+</script>
