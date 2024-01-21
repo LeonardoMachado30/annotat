4129b3d9e0c074c2a6e2794f2258de9a1a08314c
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-21 19:35:13 -0300

    ADD: Header

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="font-bold text-2xl text-gray-600 w-full text-start mt-2 mb-6 slide-top"
+  >
+    {{ title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const route = useRoute();
+const title = ref<string>("");
+console.log(route);
+
+if (route.path.includes("/addnote")) {
+  title.value = "Adicionar";
+} else {
+  title.value = "Anotações";
+}
+</script>
