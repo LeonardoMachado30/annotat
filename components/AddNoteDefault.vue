26e9113802ad734bae69dc8ed2bc2bcdbc076234
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2024-01-21 19:35:05 -0300

    ADD: Components note add

---
+++ components/AddNoteDefault.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="flex flex-col items-center">
+    <PaperClipIcon class="h-20 w-20 text-gray-500 mb-8" />
+
+    <h1 class="text-[#1b96f3] font-bold text-3xl">Anotações</h1>
+    <p class="text-gray-400">by Speedio</p>
+  </span>
+
+  <ButtonPrimary />
+</template>
+
+<script setup lang="ts">
+import { PaperClipIcon } from "@heroicons/vue/24/solid";
+import ButtonPrimary from "@@/components/Buttons/ButtonPrimary.vue";
+
+</script>
---
+++ components/inputs/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <span :html-for="title" class="text-gray-500">{{ title }}</span>
+  <div class="relative">
+    <input
+      class="px-6 h-16 placeholder:text-gray-500 w-full rounded-full focus:border-[#2E76BE] focus:!border-none focus:shadow-md focus:outline-none focus:transition focus:duration-800"
+      :placeholder="placeholder"
+      :type="type"
+    />
+    <div class="absolute right-5 top-5">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { title, placeholder } = defineProps({
+  title: { type: String },
+  placeholder: { type: String },
+  type: { type: String },
+});
+</script>
